c712ed4d6d51740101cef302af89b1481ab1c135
shinkarom <shinkarom@gmail.com>
2022-05-26 16:45:19 +0300

    Add ADDI, SB, SH, SW

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -9,6 +9,12 @@ constexpr int UJTypeMask = 0x7F;
 inttype registers[32];
 inttype pc;
 
+inttype signExtend(inttype value, int bits) {
+	assert(bits > 0 && bits < sizeof(inttype));
+	sinttype signExtendMask = 1 << (bits - 1);
+	return (value ^ signExtendMask) - signExtendMask;
+}
+
 int getRd(uint32_t instr) {
 	return (instr>>7) & 0x1F;
 }
@@ -20,6 +26,29 @@ int getRs1(uint32_t instr) {
 int getRs2(uint32_t instr) {
 	return (instr>>20) & 0x1F;
 }
+
+#define PARSE_RTYPE(opcode) \
+	} \
+	else if(((instruction) & RTypeMask) == opcode) { \
+		int rd = getRd(instruction); \
+		int rs1 = getRegister(getRs1(instruction)); \
+		int rs2 = getRegister(getRs2(instruction));
+		
+#define PARSE_ITYPE(opcode) \
+	} \
+	else if(((instruction) & ISBTypeMask) == opcode) { \
+		int rd = getRd(instruction); \
+		int rs1 = getRegister(getRs1(instruction)); \
+		int imm = signExtend(instruction >> 20, 12);
+		
+#define PARSE_STYPE(opcode) \
+	} \
+	else if(((instruction) & ISBTypeMask) == opcode) { \
+		int rs1 = getRegister(getRs1(instruction)); \
+		int rs2 = getRegister(getRs2(instruction)); \
+		int imm115 = (instruction >> 25) << 5; \
+		int imm40 = (instruction >> 7) & 0xF; \
+		int imm = signExtend(imm115|imm40, 12);
 
 inttype CCPU::getRegister(int regNumber){
 	assert(regNumber>0 && regNumber <32);
@@ -63,8 +92,7 @@ void CCPU::runOneInstruction() {
 		int imm1912 = (instruction >> 12) & 0b11111111;
 		inttype imm = ((instruction>>12) & 0b10000000010000000000)|(imm1912<<11)|imm101;
 		//sign-extend
-		sinttype signExtendMask = 1 << 19;
-		imm = (imm ^ signExtendMask) - signExtendMask;
+		imm = signExtend(imm, 20);
 		imm <<= 1;
 		pc += imm;
 	}
@@ -77,13 +105,21 @@ void CCPU::runOneInstruction() {
 		int rd = getRd(instruction); 
 		inttype imm = instruction & !UJTypeMask;
 		setRegister(rd, imm+pc-4);
-	}
-	else if ((instruction & RTypeMask) == 0x33) //ADD
-	{
-		int rd = getRd(instruction);
-		int rs1 = getRs1(instruction);
-		int rs2 = getRs2(instruction);
-		setRegister(rd, getRegister(rs1)+getRegister(rs2));		
+	// SB
+	PARSE_STYPE(0x23)
+		bus->write<uint8_t>(rs1+imm, rs2&0xFF);
+	// SH
+	PARSE_STYPE(0x1023)
+		bus->write<uint16_t>(rs1+imm, rs2&0xFFFF);
+	// SW
+	PARSE_STYPE(0x2023)
+		bus->write<uint32_t>(rs1+imm, rs2&0xFFFFFFFF);
+	// ADDi
+	PARSE_ITYPE(0x13)
+		setRegister(rd, rs1+imm);
+	// ADD
+	PARSE_RTYPE(0x33)
+		setRegister(rd, rs1+rs2);		
 	}
 	else {
 		cout<<"pc was "<<pc<<", instruction was "<<hex<<instruction<<endl;
